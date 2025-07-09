27cf771d26110ad4b563d83ae54e36f7759c137f
Asher Lien <birdym.tw@gmail.com>
2025-07-09 16:07:46 +0800

    eip

--- src/features/auth/sign-in/components/user-auth-form.tsx
+++ src/features/auth/sign-in/components/user-auth-form.tsx
@@ -44,7 +44,7 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
     mutate: SignInRequest,
     isPending: SignInRequestPending,
   } = useMutation<LoginResponse, Error, z.infer<typeof formSchema>>({
-    mutationFn: async (data: z.infer<typeof formSchema>) => {
+    mutationFn: async (data) => {
       const response = await useAuthApi.login({
         name: data.username,
         password: data.password,
@@ -116,38 +116,6 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
         >
           Login
         </Button>
-        {/*
-        <div className="relative my-2">
-          <div className="absolute inset-0 flex items-center">
-            <span className="w-full border-t" />
-          </div>
-          <div className="relative flex justify-center text-xs uppercase">
-            <span className="bg-background text-muted-foreground px-2">
-              Or continue with
-            </span>
-          </div>
-        </div>
-
-        <div className="grid grid-cols-2 gap-2">
-          <Button
-            variant="outline"
-            type="button"
-            disabled={isLoading}
-          >
-            <IconBrandGithub className="h-4 w-4" />
-            {' '}
-            GitHub
-          </Button>
-          <Button
-            variant="outline"
-            type="button"
-            disabled={isLoading}
-          >
-            <IconBrandFacebook className="h-4 w-4" />
-            {' '}
-            Facebook
-          </Button>
-        </div> */}
       </form>
     </Form>
   )
