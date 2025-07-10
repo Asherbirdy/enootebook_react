40cc930214232d216b8457b5aac0058f315eabe9
Asher Lien <birdym.tw@gmail.com>
2025-07-10 10:38:16 +0800

    更新

--- knip.config.ts
+++ knip.config.ts
@@ -1,7 +1,7 @@
 import type { KnipConfig } from 'knip'
 
 const config: KnipConfig = {
-  ignore: ['src/components/ui/**', 'src/routeTree.gen.ts'],
+  ignore: ['src/components/ui/**', 'src/routeTree.gen.ts', '/.github/**'],
   ignoreDependencies: ['tailwindcss', 'tw-animate-css'],
 }
 
